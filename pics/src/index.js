bc4e4800c824abc027766e6d3aae655bf711dff8
田原 一樹 <kazuki.tahara@rarejob.co.jp>
2019-03-20 09:31:30 +0900

    [update] make scaffold

--- pics/src/index.js
+++ pics/src/index.js
@@ -1,10 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-
-const App = () => {
-  return (
-    <div>Hither</div>
-  );
-};
+import App from './components/App'
 
 ReactDOM.render(<App />, document.getElementById('root'));
